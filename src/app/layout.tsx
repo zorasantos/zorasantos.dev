28bfa4617d60ed572a4e87b0534e91fe3ab3bd58
zorasantos <zoranildo22@gmail.com>
2023-08-23 18:02:34 -0300

    feat: implements google tag menager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Roboto } from 'next/font/google'
 import { Footer, Header } from '@/components'
 
@@ -17,7 +18,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id='gtm'
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.GA_TRACKING_ID}');
+            `
+          }}
+        />
+      </head>
       <body className={`${roboto.className} min-h-screen px-10 md:px-24`}>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${process.env.GA_TRACKING_ID}`}
+          height="0"
+          width="0"
+          style={{display: 'none', visibility: 'hidden'}}>
+        </iframe>
+      </noscript>
         <Header />
         {children}
         <Footer />
