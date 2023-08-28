5c0371a8807d5ac736781078b5b0180003f55d8f
zorasantos <zoranildo22@gmail.com>
2023-08-28 10:33:47 -0300

    feat: create theme switcher

--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -1,7 +1,17 @@
 "use client"
+import Image from "next/image";
 import { useTheme } from "next-themes";
+import DarkModeIcon from "@/assets/dark_mode.svg"
+import LightModeIcon from "@/assets/light_mode.svg"
 
 export default function ThemeSwitcher() {
   const { theme, setTheme } = useTheme()
-  return <button className="text-slate-900 dark:text-white" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>Change Theme</button>
+
+  const icon =  theme === "dark" ? LightModeIcon : DarkModeIcon
+
+  return (
+    <Image className=" bg-slate-500 dark:bg-white rounded-full cursor-pointer"
+     onClick={() => setTheme(theme === "dark" ? "light" : "dark")} src={icon}
+     alt={`${theme} mode icon`} />
+  )
 }
