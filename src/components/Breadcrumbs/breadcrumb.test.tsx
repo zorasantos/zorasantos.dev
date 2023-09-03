5bdad089d794f2090b0b9ceff6a510f21f289c75
zorasantos <zoranildo22@gmail.com>
2023-09-03 08:45:01 -0300

    test: breadcrumb

--- src/components/Breadcrumbs/breadcrumb.test.tsx
+++ src/components/Breadcrumbs/breadcrumb.test.tsx
@@ -1,17 +1,21 @@
-import { vi, expect, describe, test } from "vitest";
+import { expect, describe, test } from "vitest";
 import { render, screen } from "@/utils/test"
 import Breadcrumb from "."
 
-describe("Header component", () => {
+describe("Breadcrumb component", () => {
   test("Should be able renders the Breadcrumb correctly", () => {
-    render(<Breadcrumb page="blog" title="Test Article Title" />)
+    render(<Breadcrumb page="Blog" title="Test Article Title" />)
 
     const logo = screen.getAllByRole("img", { name: "icon home" })
     const icon = screen.getAllByRole("img", { name: "icon chevron right" })
-    const link = screen.getAllByRole("link", { name: "blog" })
+    const link = screen.getAllByRole("link", { name: "Blog" })
+    const linkIcon = screen.getAllByRole("link", { name: "icon home" })
 
     expect(logo).toBeDefined()
     expect(icon).toBeDefined()
     expect(link).toBeDefined()
+    expect(link[0].getAttribute('href')).toBe('/')
+    expect(linkIcon[0].getAttribute('href')).toBe('/')
+    expect(logo[0].getAttribute('alt')).toBe('icon home')
   })
 })
