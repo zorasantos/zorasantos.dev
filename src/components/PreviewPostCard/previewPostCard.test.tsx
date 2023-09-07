7a91c020cefd000148a032371d157fb0f6fda8b3
zorasantos <zoranildo22@gmail.com>
2023-09-07 11:27:34 -0300

    test(unit): preview post card

---
+++ src/components/PreviewPostCard/previewPostCard.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@/utils/test';
+import PreviewPostCard from '.';
+
+describe('Post details Component', () => {
+  test('Should be able renders post detail correctly', () => {
+    const post =
+      {
+        author: "Test author",
+        title: "Test Title",
+        publishedAt: "01/01/2023",
+        description: "Test Description",
+        tags: ["api", "node"],
+        slug: "test-title"
+      }
+
+    render(<PreviewPostCard { ...post } />);
+
+    const image = screen.getByRole("img", { name: "photo author" })
+    const publishedAt = screen.getByLabelText("Data de publicação").getAttribute('aria-label')
+    const title = screen.getByRole("heading", { name: "Test Title" })
+    const description = screen.getByLabelText("Descrição").getAttribute('aria-label')
+
+    expect(image).toBeDefined()
+    expect(publishedAt).toBeDefined()
+    expect(title).toBeDefined()
+    expect(description).toBeDefined()
+  });
+})
