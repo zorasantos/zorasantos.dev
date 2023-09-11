3c1f387582f9f2263727011e68a30764bb700821
zorasantos <zoranildo22@gmail.com>
2023-09-11 00:35:22 -0300

    chore: include text in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,9 +8,7 @@ export const metadata: Metadata = {
 export default function About() {
   return (
     <>
-      <div className="flex items-center gap-3 mb-10">
-        <h3 className="text-2xl font-bold">Sobre Mim</h3>
-      </div>
+      <h3 className="text-2xl font-bold mb-8">Sobre Mim</h3>
       <div className="flex flex-col gap-5 text-justify text-lg text-[#2D3748]">
         <p>
           Olá! me chamo <strong className="dark:text-primary-dark">Zoranildo Santos</strong>, um apaixonado por desenvolvimento de software, com uma ampla experiência
@@ -19,7 +17,7 @@ export default function About() {
         </p>
         <p>
           Minha abordagem é focada na construção de soluções eficientes e escaláveis, sempre mantendo a usabilidade
-          e a experiência do usuário como prioridade. Fui muito impactado pela primeira aplicação que desenvolvi na minha carreira,
+          e a experiência do usuário como prioridade. Fui muito impactado pela primeira aplicação em que atuei na minha carreira,
           trata-se do <strong className="dark:text-primary-dark">Portal do Contribuinte</strong>, um app para uso da população em geral, que revolucionou a relação dos
           contribuintes com a <strong className="dark:text-primary-dark">Procuradoria Geral do Estado do Ceará</strong>. Com o desenvolvimento do Portal tornou-se
           possível ao contribuinte, por exemplo, marcar reuniões e fazer requerimentos, que antes só era possível presencialmente.
@@ -28,6 +26,17 @@ export default function About() {
           Além do código, tenho um forte interesse em arquitetura de software. Minha paixão pelo desenvolvimento web
           me impulsiona a explorar novas tecnologias e tendências para estar sempre à frente no cenário dinâmico da web.
         </p>
+      </div>
+      <h3 className="text-2xl font-bold mb-8 mt-8">Formação Acadêmica</h3>
+      <div className="flex flex-col gap-5 text-justify text-lg text-[#2D3748]">
+        <div className="flex flex-col">
+          <span><strong>Pós-graduação - Arquitetura de Soluções</strong>, em andamento (Iniciado em 09/2023) </span>
+          <span><strong>PUC/Minas</strong> - Pontifícia Universidade Católica de Minas Gerais</span>
+        </div>
+        <div className="flex flex-col">
+          <span><strong>Licenciatura em Filosofia</strong>,  concluído em 2017</span>
+          <span><strong>UFC</strong> - Universidade Federal do Ceará</span>
+        </div>
       </div>
     </>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,19 +17,19 @@ export default function Header() {
   return (
   <header className={`sticky  ${isHidden ? 'top-[-6rem]' : `top-0 ${isReachedTop ? '' : 'shadow-lg'}`} z-10 flex justify-between items-center mb-10 h-24 px-10 md:px-24 backdrop-blur-xl transition-all duration-500 ease-in-out`}>
     <Link data-cy="logo" rel="noopener" tabIndex={0} href="/">
-      <Image priority alt="logo" src={Logo} width={80} height={64} className="w-20 h-16" />
+      <span className="hover:text-primary-light dark:hover:text-primary-light font-semibold cursor-pointer">Zora Santos</span>
     </Link>
     <div className="flex gap-5">
     {NavItems?.map(item => (
-        <Link
-          rel="noopener"
-          data-cy={item.dataCy}
-          key={item.pathname}
-          tabIndex={0}
-          href={item.pathname}
-          className="text-gray-700 dark:text-white font-semibold cursor-pointer hover:text-primary-light dark:hover:text-primary-light">
-            {item.menuName}
-          </Link>
+      <Link
+        rel="noopener"
+        data-cy={item.dataCy}
+        key={item.pathname}
+        tabIndex={0}
+        href={item.pathname}
+        className="dark:text-white font-semibold cursor-pointer hover:text-primary-light dark:hover:text-primary-light">
+          {item.menuName}
+        </Link>
       ))}
       <ThemeSwitcher />
     </div>
