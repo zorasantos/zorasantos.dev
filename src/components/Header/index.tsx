9f22569be5d3a38d10b117bebfc0276dd47a23cf
zorasantos <zoranildo22@gmail.com>
2023-09-11 00:40:29 -0300

    test(fix): header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,7 +17,7 @@ export default function Header() {
   return (
   <header className={`sticky  ${isHidden ? 'top-[-6rem]' : `top-0 ${isReachedTop ? '' : 'shadow-lg'}`} z-10 flex justify-between items-center mb-10 h-24 px-10 md:px-24 backdrop-blur-xl transition-all duration-500 ease-in-out`}>
     <Link data-cy="logo" rel="noopener" tabIndex={0} href="/">
-      <span className="hover:text-primary-light dark:hover:text-primary-light font-semibold cursor-pointer">Zora Santos</span>
+      <span aria-label="logo" className="hover:text-primary-light dark:hover:text-primary-light font-semibold cursor-pointer">Zora Santos</span>
     </Link>
     <div className="flex gap-5">
     {NavItems?.map(item => (
